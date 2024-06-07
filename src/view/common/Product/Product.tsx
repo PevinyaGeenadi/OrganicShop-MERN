d1897162a4bb1112920ee210ef7aaf076d3f0d34
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-06-07 23:13:43 +0530

    update

--- src/view/common/Product/Product.tsx
+++ src/view/common/Product/Product.tsx
@@ -1,36 +1,31 @@
-import {Component} from "react";
+import { Component } from "react";
 import spinash from "../../../images/products/spinach.png";
 
 interface ProductProps {
     data: any;
 }
-export class Product extends Component<ProductProps>{
 
+export class Product extends Component<ProductProps> {
     render() {
-
-    const {data} = this.props;
-    const image = require('../../../images/products/' + data.image)
-    return (
-        <div className="w-48 h-64 mr-4 mb-4 justify-center items-center border-gray-500 border-[0.5px]">
-            <div>
-                <img className="h-[128px] w-[128px]" src={image} alt=""/>
-            </div>
-            <div className="flex">
-                <div>
-                    <h3 className="text-secondary text-[16px] pl-2">{data.name}</h3>
+        const { data } = this.props;
+        const image = require('../../../images/products/' + data.image);
+        return (
+            <div className="w-36 h-44 mr-4 mb-4 justify-center items-center border-gray-500 border-[0.5px]">
+                <div className="flex justify-center">
+                    <img className="h-[112px] w-[112px]" src={image} alt={data.name} />
                 </div>
-                <div className="bg-yellow-300 ml-1 p-1 rounded-lg pr-2">
-                    <h3 className="text-[16px] pl-2">{data.price} <small className="text-[7px]">{data.currency}</small></h3>
+                <div className="flex mt-2">
+                    <div>
+                        <h3 className="text-secondary text-[14px] pl-2">{data.name}</h3>
+                    </div>
+                    <div className="bg-yellow-300 ml-1 p-1 rounded-lg pr-2">
+                        <h3 className="text-[14px] pl-2">{data.price} <small className="text-[8px]">{data.currency}</small></h3>
+                    </div>
+                </div>
+                <div className="flex justify-center mt-2">
+                    <button className="w-full p-[4px] bg-secondary text-[10px] border-gray-500 border-[0.5px]">Add to Cart</button>
                 </div>
             </div>
-            <div className="flex justify-center">
-                <button className="w-full mt-1 p-[2.4px] bg-secondary text-[12px] border-gray-500 border-[0.5px]">Add to Cart</button>
-            </div>
-        </div>
-    );
+        );
+    }
 }
-}
-// Path: src/view/common/Product/Product.tsx
-
-
-
