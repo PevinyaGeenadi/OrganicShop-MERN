5c151ec912b96133ddd352d9e766cb7436db008c
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-06-11 21:03:47 +0530

    Updated footer and navbar

--- client/src/view/common/Footer/Footer.tsx
+++ client/src/view/common/Footer/Footer.tsx
@@ -4,18 +4,13 @@ import icon from '../../../images/Organic Shop Logo.png';
 export class Footer extends Component {
     render() {
         return (
-            <div className="p-2
-                       bg-[#444544]
-                       flex
-                       justify-center
-
-                       w-full
-                       fixed
-                       bottom-0">
-                <p className="pt-1 pr-3 text-[10px] text-[#e6f0e6] hover:text-green-400">Copyright © 2023</p>
+            <div className="p-2 bg-[#444544] flex justify-center w-full fixed bottom-0">
+                <p className="pt-1 pr-3 text-[10px] text-[#e6f0e6] hover:text-green-400">Copyright © 2024</p>
                 <h1 className="text-1xl text-secondary">Organic Shop</h1>
                 <img className="h-5 w-5 top-0 ml-1" src={icon} alt=""/>
             </div>
+
+
         );
     }
 }
--- client/src/view/common/Navbar/NavBar.tsx
+++ client/src/view/common/Navbar/NavBar.tsx
@@ -11,16 +11,20 @@ export class NavBar extends Component {
                     text-secondary">Organic Shop
                     </h1>
                     <img className="h-6 w-6 ml-1" src={icon} alt=" "/>
-                   <ul className="list-none ml-[790px] ">
-                       <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/">Home</Link> </li>
-                       <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/about">About</Link></li>
-                       <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/contact">Contact</Link></li>
-                       <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/shopping-cart">My-Cart</Link></li>
-                   </ul>
-                    <button className="text-[14px] text-[#e6f0e6] bg-green-400 pl-3 pr-3 rounded hover:text-tertiary">
+                    <ul className="list-none ml-[790px] ">
+                        <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/">Home</Link> </li>
+                        <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/about">About</Link></li>
+                        <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/contact">Contact</Link></li>
+                        <li className="inline-block mr-5 text-[#e6f0e6] cursor-pointer hover:text-green-500"><Link to="/shopping-cart">My-Cart</Link></li>
+                    </ul>
+
+                    <button className="text-[14px] text-[#e6f0e6] bg-green-700 pl-3 pr-3 rounded-lg hover:bg-green-600 transition duration-300">
                         <Link to="/login">Sign In</Link>
 
-                        </button>
+                    </button>
+
+
+
                 </div>
             </>
         );
