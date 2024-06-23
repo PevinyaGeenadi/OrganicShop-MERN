b9cfe630d7bd455284062cb92e24f557678485de
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-06-23 22:24:29 +0530

    update

---
+++ server/routes/ContactRoutes.js
@@ -0,0 +1,14 @@
+const express
+    = require('express');
+const router
+    = express.Router();
+
+router.post('/submit',(req, res) => {
+    const requestBody
+        = req.body;
+    console.log(requestBody);
+    res.send("Contact " +
+        "details received!");
+});
+
+module.exports = router;
